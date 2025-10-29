42577b777786f30dbe019a74ae5a730776983175
sebastien_chapert <sebastien.chappert@hotmail.fr>
2025-10-29 11:33:15 +0100

    Ajout domain cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: ['http://localhost:5173'], // A adpater au front React
+    origin: [
+      'http://localhost:5173', // dev local
+      'https://1-demo-project-front-react-xqdi.vercel.app',
+    ], // A adpater au front React
     credentials: true, // autorise les cookies pour setter le JWT http-only
   });
   await app.listen(process.env.PORT ?? 3000);
